4939efb1154b309cf8af6ecee2f4cac9a612bf9d
393846671 <393846671@qq.com>
2014-04-22 23:26:55 +0800

    Revert "add new"
    This reverts commit 7f193288cd8f445cbc1e72135b5cd87cbed464e3.

--- GitHubTest/GitHubTest/GitAppDelegate.m
+++ GitHubTest/GitHubTest/GitAppDelegate.m
@@ -14,6 +14,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    NSLog(@"i do something change");
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
