a7aaa43ebce629a0ed290d477878cdf38c7bf04a
393846671 <393846671@qq.com>
2014-04-22 23:22:15 +0800

    submit
    hello git

---
+++ GitHubTest/GitHubTest/GitAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GitAppDelegate.h
+//  GitHubTest
+//
+//  Created by mao lin on 14-4-22.
+//  Copyright (c) 2014年 mao lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubTest/GitHubTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubTest
+//
+//  Created by mao lin on 14-4-22.
+//  Copyright (c) 2014年 mao lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GitAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GitAppDelegate class]));
+    }
+}
